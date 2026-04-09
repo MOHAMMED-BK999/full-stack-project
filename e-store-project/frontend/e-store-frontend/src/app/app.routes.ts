04ca2153e7283a1392ac3ddd16d146726b73daa0
MOHAMMED-BK999 <mohamedbarik2005@gmail.com>
2026-04-09 13:30:56 +0100

    catalog

--- e-store-project/frontend/e-store-frontend/src/app/app.routes.ts
+++ e-store-project/frontend/e-store-frontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { Catalog } from './catalog/catalog';
 import { Accueil } from './accueil/accueil';
 import { Commands } from './commands/commands';
+import { Connexion } from './connexion/connexion';
 
 export const routes: Routes = [
   { path: '', component: Accueil },
   { path: 'accueil', component: Accueil },
   { path: 'catalog', component: Catalog },
   { path: 'commands', component: Commands },
+  { path: 'connexion', component: Connexion },
 
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
